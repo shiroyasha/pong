05dfb99f158329760ba5ebb369b531f7211f348c
shiroyasha <igisar@gmail.com>
2013-02-24 18:51:00 +0100

    kinetic

--- js/main.js
+++ js/main.js
@@ -1,55 +1,51 @@
 
-var Person = Class.extend({
-
-	init: function() {
-		console.log("pocetak");
-		this.ime = 'Pera';
-		this.adresa = 'perislava 12';
-	},
-
-	pevaj: function() {
-		console.log( this.ime + this.adresa );
-	}
-
-});
-
-var Renderer = Class.extend({
-	init: function() {
-		requestAnimFrame( this.render() );
-
-		this.paddle = { x : 100, y : 100 };
-	},
-
-	render: function() {
-		
-	}
-});
 
 
 APP = {
 	start: function() {
+
+		console.log(window.Kinetic);
 		
-		var person1 = new Person();
+	  var stage = new Kinetic.Stage({
+        container: 'container',
+        width: 578,
+        height: 200
+      });
 
-		person1.pevaj();
+      var layer = new Kinetic.Layer();
 
+      window.rect = new Kinetic.Rect({
+        x: 239,
+        y: 75,
+        width: 100,
+        height: 50,
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 4
+      });
+
+      console.log(rect);
+
+      // add the shape to the layer
+      layer.add(rect);
+
+      // add the layer to the stage
+      stage.add(layer);
+
+      window.direction = 1;
+
+      var anim = new Kinetic.Animation(function(frame) {
+      	rect.attrs.x = ( rect.attrs.x + direction * ((Math.floor(500 * frame.timeDiff/1000)) ) ) % 478;
+      }, layer);
+
+       anim.start();
+	},
+
+	render: function(frame) {
+		
+		
 	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
